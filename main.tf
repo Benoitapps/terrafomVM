2be26079f00b5868adf9da9428bd271bfc83f251
Nicolas CAPELLA <colas.capel73@gmail.com>
2024-05-02 17:09:01 +0200

    Add variables for project name, admin username, and admin password

--- main.tf
+++ main.tf
@@ -1,54 +1,69 @@
+variable "project_name" {
+  type    = string
+  default = "devops"
+}
+
+variable "admin_username" {
+  type    = string
+  default = "adminuser"
+}
+
+variable "admin_password" {
+  type    = string
+  default = "YourPassword123!"
+}
+
 provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+resource "azurerm_resource_group" "setup" {
+  name     = "${var.project_name}-rg"
   location = "West Europe"
 }
 
-resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
+resource "azurerm_virtual_network" "network" {
+  name                = "${var.project_name}-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.setup.location
+  resource_group_name = azurerm_resource_group.setup.name
 }
 
-resource "azurerm_subnet" "example" {
-  name                 = "example-subnet"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
+resource "azurerm_subnet" "subnet" {
+  name                 = "${var.project_name}-subnet"
+  resource_group_name  = azurerm_resource_group.setup.name
+  virtual_network_name = azurerm_virtual_network.network.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-resource "azurerm_public_ip" "example" {
-  name                = "example-public-ip"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_public_ip" "public_ip" {
+  name                = "${var.project_name}-public-ip"
+  location            = azurerm_resource_group.setup.location
+  resource_group_name = azurerm_resource_group.setup.name
   allocation_method   = "Dynamic"
 }
 
-resource "azurerm_network_interface" "example" {
-  name                      = "example-nic"
-  location                  = azurerm_resource_group.example.location
-  resource_group_name       = azurerm_resource_group.example.name
+resource "azurerm_network_interface" "network_interface" {
+  name                = "${var.project_name}-nic"
+  location            = azurerm_resource_group.setup.location
+  resource_group_name = azurerm_resource_group.setup.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.example.id
+    subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.example.id
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
   }
 }
 
-resource "azurerm_linux_virtual_machine" "example" {
-  name                            = "example-machine"
-  location                        = azurerm_resource_group.example.location
-  resource_group_name             = azurerm_resource_group.example.name
-  network_interface_ids           = [azurerm_network_interface.example.id]
+resource "azurerm_linux_virtual_machine" "virtual_machine" {
+  name                            = "${var.project_name}-vm"
+  location                        = azurerm_resource_group.setup.location
+  resource_group_name             = azurerm_resource_group.setup.name
+  network_interface_ids           = [azurerm_network_interface.network_interface.id]
   size                            = "Standard_DS1_v2"
-  admin_username                  = "adminuser"
-  admin_password                  = "YourPassword123!" 
+  admin_username                  = var.admin_username
+  admin_password                  = var.admin_password
   disable_password_authentication = false
 
   os_disk {
@@ -64,24 +79,30 @@ resource "azurerm_linux_virtual_machine" "example" {
   }
 }
 
-output "public_ip_address" {
-  value = azurerm_public_ip.example.ip_address
+data "azurerm_public_ip" "public_ip" {
+  name                = azurerm_public_ip.public_ip.name
+  resource_group_name = azurerm_linux_virtual_machine.virtual_machine.resource_group_name
 }
 
+
+
 resource "null_resource" "install_figlet" {
- 
+  provisioner "remote-exec" {
     connection {
       type     = "ssh"
-      user     = "adminuser"
-      host     = azurerm_public_ip.example.ip_address
-      password = "YourPassword123!" 
+      user     = var.admin_username
+      host     = data.azurerm_public_ip.public_ip.ip_address
+      password = var.admin_password
       port     = 22
     }
-
-    provisioner "remote-exec" {
-       inline = [
+    inline = [
       "mkdir test",
     ]
-
   }
+
+  depends_on = [azurerm_linux_virtual_machine.virtual_machine, azurerm_public_ip.public_ip]
+}
+
+output "public_ip_address" {
+  value = azurerm_public_ip.public_ip.ip_address
 }
